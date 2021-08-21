14d68fc9b967277e4fff1463885505082ef43618
JavierAmaya <photojavhn@gmail.com>
2021-08-21 00:34:46 -0600

    adding dinamic data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,9 @@ import { UsuariosComponent } from './components/usuarios/usuarios.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OrdenCaminoComponent } from './components/orden-camino/orden-camino.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 
 
 
@@ -56,7 +59,13 @@ import { OrdenCaminoComponent } from './components/orden-camino/orden-camino.com
     FontAwesomeModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
